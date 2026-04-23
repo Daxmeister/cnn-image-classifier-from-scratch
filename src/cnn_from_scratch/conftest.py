dadbcdab1479835f9dec853622e0ff22722cb146
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2026-04-23 16:18:20 +0200

    tests: refactors test. Introduces fixtures, separates convolver tests form Debug_data object.

---
+++ src/cnn_from_scratch/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+import debug_data
+import convolver
+import cnn
+
+@pytest.fixture
+def dbg():
+    return debug_data.Debug_data()
+
+@pytest.fixture
+def convolver_obj():
+    return convolver.Convolver()
+
+@pytest.fixture
+def cnn_obj(convolver_obj):
+    return cnn.CNN(convolver_obj)
+
+@pytest.fixture
+def epsilon():
+    return 1e-6
